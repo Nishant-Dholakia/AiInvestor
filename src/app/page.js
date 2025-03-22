8297d2fcacdcf7304a596a5ff7282bdfb31d3060
Nishant Dholakia <nishantdholakia2020@gmail.com>
2025-03-22 15:09:18 +0530

    added sidebar

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import { HomePage } from './component/HomePage'
+import {SidebarDemo } from "./component/Sidebar";
+
 function page() {
   return (
     <>
-    <HomePage/>
+      <SidebarDemo />
+
+      <HomePage/>
     </>
   )
 }
