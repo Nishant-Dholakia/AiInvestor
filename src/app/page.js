cb338dfb1bfb3835049e987915799f840cf427cb
Devan019 <devanchauhan012@gmail.com>
2025-03-22 15:03:28 +0530

    add auth

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import { HomePage } from './component/HomePage'
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
 function page() {
   return (
     <>
-    <HomePage/>
+      <HomePage />
     </>
   )
 }
